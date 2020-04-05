363c10389c22a6b50b52cdfb199c62d1ad93cd2f
virus008 <sonavalevrushabh@gmail.com>
2020-04-05 16:22:38 +0530

    Updates

---
+++ precache-manifest.d12ea0031df6b0478c638f5fcdaaf45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74165d15ab5b543643e5f61d9c362b72",
+    "url": "/memory-router/index.html"
+  },
+  {
+    "revision": "1bdbe4c3c1bee221ba80",
+    "url": "/memory-router/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e7a5d71d439ac77d4de",
+    "url": "/memory-router/static/js/2.60495ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-router/static/js/2.60495ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdbe4c3c1bee221ba80",
+    "url": "/memory-router/static/js/main.64a9e31a.chunk.js"
+  },
+  {
+    "revision": "d99f00f5cb193d054adb",
+    "url": "/memory-router/static/js/runtime-main.1b94b6b8.js"
+  }
+]);
